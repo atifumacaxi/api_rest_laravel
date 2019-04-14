80647daffbe431386d2974ee2f5272b919ad2934
unknown <atifumacaxi@gmail.com>
2019-04-14 20:53:42 -0300

    Adjustments for travis testing.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/users');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
